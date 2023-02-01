8941b1e11f9e8c56839890ab17d93c9566bb4b3a
Jacky Song <almey0721@gmail.com>
2023-02-01 15:51:46 -0500

    Updated fragshader to new API

--- examples/fragshader.rs
+++ examples/fragshader.rs
@@ -1,81 +1,144 @@
-use std::env;
-use std::fs::read_to_string;
-use std::time::Instant;
-
 // Takes almost any fragment shader as input and renders with the shader
 // should (eventually) run almost any shader on Shadertoy
-use elara_gfx::gfx::*;
-use elara_gfx::{Event, EventLoop, GLWindow};
-use elara_log::Logger;
+use elara_gfx::{gl_info, Buffer, BufferType, Program, Shader, Uniform, VertexArray};
+use elara_gfx::{GLWindow, HandlerResult, WindowHandler};
+use elara_log::prelude::*;
+use std::error::Error;
+use std::fs::read_to_string;
+use std::process::exit;
+use std::time::{Duration, Instant};
 
-fn main() -> GfxResult {
-    let mut log = Logger::new();
-    log.info("Program starting...");
+const DUMMY_VERTEX_SHADER: &'static str = include_str!("shaders/quad.vert");
+
+struct Handler {
+    vao: VertexArray,
+    program: Program,
+    resolution: (f32, f32),
+    frame_count: u32,
+    start_time: Instant,
+}
+
+fn show_render_stats(frame_interval: u32, current_frame: u32, frame_render_time: Duration) {
+    // Shows render stats once per <frame_interval> frames
+    if current_frame % frame_interval == 0 {
+        debug!(
+            "Frame {} average render time {:?}",
+            current_frame, frame_render_time
+        );
+    }
+}
+
+impl Handler {
+    fn new(win: &GLWindow, shader_src: String) -> Result<Handler, String> {
+        let start_time = Instant::now();
+        let frame_count = 0;
+        let resolution = (win.width() as f32, win.height() as f32);
+        let vertex_shader = Shader::new(&DUMMY_VERTEX_SHADER, gl::VERTEX_SHADER)?;
+        let fragment_shader = Shader::new(&shader_src, gl::FRAGMENT_SHADER)?;
+        let program = Program::new(&[vertex_shader, fragment_shader])?;
+        program.use_program();
+
+        // Render 1 fullscreen quad for shaders
+        #[rustfmt::skip]
+        let vertices = [
+            1.0_f32, 1.0, 0.0, // top right
+            1.0, -1.0, 0.0, // bottom right
+            -1.0, -1.0, 0.0, // bottom left
+            -1.0, 1.0, 0.0, // top left
+        ];
+
+        #[rustfmt::skip]
+        let indices = [
+            0, 1, 3,
+            1, 2, 3
+        ];
+
+        let vao = VertexArray::new()?;
+        let vbo = Buffer::new()?;
+        let ebo = Buffer::new()?;
+
+        vao.bind();
+        vbo.bind(BufferType::Array);
+        vbo.data::<f32>(BufferType::Array, &vertices, gl::STATIC_DRAW);
+        ebo.bind(BufferType::ElementArray);
+        ebo.data::<i32>(BufferType::ElementArray, &indices, gl::STATIC_DRAW);
+        vao.vertex_attrib_pointer(0, 3, gl::FLOAT, false, 0);
+        vao.enable_vertex_attrib(0);
+        vbo.unbind(BufferType::Array);
+        vao.unbind();
+
+        Ok(Handler {
+            vao,
+            program,
+            resolution,
+            frame_count,
+            start_time,
+        })
+    }
+    fn current_frame(&self) -> u32 {
+        self.frame_count
+    }
+    fn add_frame(&mut self) {
+        self.frame_count += 1
+    }
+    fn get_elapsed_time(&self) -> Duration {
+        self.start_time.elapsed()
+    }
+}
+
+impl WindowHandler for Handler {
+    fn on_draw(&mut self) -> HandlerResult<()> {
+        unsafe {
+            let res_uniform = Uniform::new(self.program.id(), "u_resolution")?;
+            let time_uniform = Uniform::new(self.program.id(), "u_time")?;
+
+            let now = std::time::Instant::now();
+            let elapsed_time = now.duration_since(self.start_time).as_secs_f32();
+
+            gl::Uniform2f(res_uniform.id(), self.resolution.0, self.resolution.1);
+            gl::Uniform1f(time_uniform.id(), elapsed_time);
+
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            self.vao.bind();
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
+            self.vao.unbind();
+        }
+        self.add_frame();
+        let current_frame = self.current_frame();
+        let frame_render_time = self.get_elapsed_time() / current_frame;
+        // Avoid showing render time counter too often so we show only once
+        // per every few hundred frames
+        show_render_stats(500, current_frame, frame_render_time);
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    Logger::new().init().unwrap();
+    info!("Starting logging...");
 
     // Command-line parsing
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args: Vec<String> = std::env::args().skip(1).collect();
     if args.len() < 1 {
-        log.error("Usage: fragshader input.frag")
+        error!("Usage: fragshader input.frag");
+        exit(1);
     }
     let shader_path = &args[0];
 
-    log.info(format!("Attempting to load shader {}", &shader_path).as_str());
-    let fs: String = read_to_string(&shader_path)?.parse()?;
+    info!("Attempting to load shader {}", &shader_path);
+    let shader_src: String = read_to_string(&shader_path)?.parse()?;
 
-    // Begin event loop and create window
-    let event_loop = EventLoop::new();
-    let window = GLWindow::new(900, 600, "Window 1", true, &event_loop);
-    window.init_gl();
-    log.info(
-        format!(
-            "Window dimensions: {} x {}",
-            window.width(),
-            window.height()
-        )
-        .as_str(),
-    );
+    let (app, window) = GLWindow::new_with_title("OpenGL shaders")?;
+    window.get_context()?;
+    gl_info();
 
-    // OpenGL info
-    gfxinfo();
-
-    let vertices = vec![
-        -1.0, 1.0, 1.0, 0.0, 0.0, // Top-left
-        1.0, 1.0, 0.0, 1.0, 0.0, // Top-right
-        1.0, -1.0, 0.0, 0.0, 1.0, // Bottom-right
-        -1.0, -1.0, 1.0, 1.0, 1.0, // Bottom-left
-    ];
-
-    let elements = vec![0, 1, 2, 2, 3, 0];
-
-    let renderer = ElementRenderer::new(vertices, elements, get_dummy_vs(), &fs);
-    renderer.attribute("position", 2, 5, null_ptr::<f32>());
-
-    let resolution = [window.width() as f32, window.height() as f32];
-    let start_time = Instant::now();
+    // Run all OpenGL calls that only
+    // needs to be run once in advance
+    // of rendering to improve performance
+    let render_handler = Handler::new(&window, shader_src)?;
 
     // Event handling
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                event: winit::event::WindowEvent::CloseRequested,
-                ..
-            } => {
-                log.info("Close request received, exiting...");
-                control_flow.set_exit();
-            }
-            Event::MainEventsCleared => {
-                // Render function
-                window.make_current();
-                let now = std::time::Instant::now();
-                let elapsed_time = now.duration_since(start_time).as_secs_f32();
-                renderer.uniform_f32("u_time", &[elapsed_time]);
-                renderer.uniform_f32("u_resolution", &resolution);
-                renderer.draw(6);
-                window.swap_buffers();
-                window.make_not_current();
-            }
-            Event::RedrawRequested(_) => {}
-            _ => {}
-        }
-    });
+    app.run_loop(window, render_handler);
+    Ok(())
 }
